7bf910144988121fe409090e83aab3df6c5e3b2d
Dilan Shanuka <99383107+IT21182914@users.noreply.github.com>
2023-03-07 05:11:29 +0000

    Add DeleteLink method

--- LinkedList.java
+++ LinkedList.java
@@ -1,83 +1,79 @@
 
 public class LinkedList {
 
-	private Link first;
-	
-	
-	public LinkedList() {
-		
-		this.first=null;
-		
-		
-	}
-	
-	public boolean isEmpty() {
-		
-		
-		return (first==null);
-		
-	}
-	public void displayList() {
-		
-		
-		Link cur = first;
-		
-		while(cur!=null) {
-			
-			cur.displayDetails();
-			cur=cur.next;
-		}
-	}
-	
-	
-	public void insertFirst(String name,double avg) {
-		
-		Link newLink = new Link(name,avg);
-		newLink.next = first;
-		first = newLink;
-		
-	}
-	
-	public Link deleteFirst()
-	{
-		
-		Link temp = first;
-		first = first.next;
-		
-		return temp;
-		
-	}
-	
-	public Link deleteLink(String name) {
-		
-		Link  cur=first;
-		Link  previous=first;
-		
-		if(cur==null) {
-	
-	      return null;
-		
-	}
-		while(!cur.name.equals(name)) {
-			
-			previous = cur;
-			cur = cur.next;
-			
-			if(cur==null) {
-				
-				return null;
-			}
-			
-		}
-	if(cur==first) {
-		
-		first=first.next;
-		
-	}else {
-		
-		previous.next = cur.next;
-	}
-	
-	return cur;
-}
+  private Link first;
+
+  public LinkedList() {
+
+    this.first = null;
+
+  }
+
+  public boolean isEmpty() {
+
+    return (first == null);
+
+  }
+
+  public void displayList() {
+
+    Link cur = first;
+
+    while (cur != null) {
+
+      cur.displayDetails();
+      cur = cur.next;
+    }
+  }
+
+  public void insertFirst(String name, double avg) {
+
+    Link newLink = new Link(name, avg);
+    newLink.next = first;
+    first = newLink;
+
+  }
+
+  public Link deleteFirst() {
+
+    Link temp = first;
+    first = first.next;
+
+    return temp;
+
+  }
+
+  // delete a link
+  public Link deleteLink(String name) {
+
+    Link cur = first;
+    Link previous = first;
+
+    if (cur == null) {
+
+      return null;
+
+    }
+    while (!cur.name.equals(name)) {
+
+      previous = cur;
+      cur = cur.next;
+
+      if (cur == null) {
+
+        return null;
+      }
+
+    }
+    if (cur == first) {
+
+      first = first.next;
+
+    } else {
+
+      previous.next = cur.next;
+    }
+
+    return cur;
+  }
 }
